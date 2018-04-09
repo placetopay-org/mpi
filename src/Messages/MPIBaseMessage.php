bc6e9ee3e874343630f7bdb75d8b6be6c3e00447
Diego Calle <dnetix@gmail.com>
2018-04-08 23:15:12 -0500

    Add message as error on response

--- src/Messages/MPIBaseMessage.php
+++ src/Messages/MPIBaseMessage.php
@@ -14,6 +14,9 @@ abstract class MPIBaseMessage
         if (isset($result['error_number']) || isset($result['error_description'])) {
             throw new ErrorResultMPI($result['error_description'], $result['error_number']);
         }
+        if (isset($result['message'])) {
+            throw new ErrorResultMPI($result['message']);
+        }
     }
 
 }
