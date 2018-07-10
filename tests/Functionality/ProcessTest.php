25fda36177aac3169cb6fa49b5377d221b84b3df
Diego Calle <dnetix@gmail.com>
2018-07-10 15:19:19 -0500

    Add disable redirect test condition on mocked client

---
+++ tests/Functionality/ProcessTest.php
@@ -0,0 +1,40 @@
+<?php
+
+
+class ProcessTest extends BaseTestCase
+{
+    public function create($overwrite = [])
+    {
+        return new \PlacetoPay\MPI\MPIService(array_merge([
+            'url' => 'https://dev.placetopay.ec/3ds-mpi/',
+            'apiKey' => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6Ijk4ZTFmMTVmZTk1ZDg4NTE4NjEyYTFhMjBlNDBmODA2ODA1MDllOWQ3Y2I1Yzg5ZTc3ZmRhY2U3OTRhMjZhZjk5ZjBjNjI5ZTc4MjZkZTVhIn0.eyJhdWQiOiIxIiwianRpIjoiOThlMWYxNWZlOTVkODg1MTg2MTJhMWEyMGU0MGY4MDY4MDUwOWU5ZDdjYjVjODllNzdmZGFjZTc5NGEyNmFmOTlmMGM2MjllNzgyNmRlNWEiLCJpYXQiOjE1MjU4MDcwMDYsIm5iZiI6MTUyNTgwNzAwNiwiZXhwIjoxNTU3MzQzMDA2LCJzdWIiOiIyIiwic2NvcGVzIjpbXX0.e4tvpy2tCXOsoz3fDhD8QI4guFZOmLq0TIv4491F1vi8WGGtxAT-68E5pJXHzKwocpF8mBDSz2Rlhmuypktv9nNEVTFPp18A7hReZlsjMqumOiUxCDF4YCejcfQOrY2NVxIvB8yrnQBaYXFD8au2EXfjAeSsOnrPjqZoTsK3XXY',
+        ], $overwrite));
+    }
+
+    public function testItObtainsAQuerySuccessfully()
+    {
+        $mpi = $this->create();
+
+        $response = $mpi->lookUp([
+            'card' => [
+                'number' => '4012001037141112',
+                'expirationYear' => '20',
+                'expirationMonth' => '12',
+            ],
+            'amount' => 1200,
+            'currency' => 'COP',
+            'redirectUrl' => 'https://dnetix.co/ping/3ds',
+        ]);
+
+        var_dump($response);
+    }
+
+//    public function testItObtainsAQuerySuccessfully()
+//    {
+//        $mpi = $this->create();
+//
+//        $response = $mpi->query(49);
+//        var_dump($response);
+//    }
+
+}
