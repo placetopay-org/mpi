fbd195d7606ab9596076de453772212a9eb45c9e
Diego Calle <dnetix@gmail.com>
2019-08-29 22:06:41 -0500

    Load basic update transaction result informaiton

--- src/Messages/UpdateTransactionResponse.php
+++ src/Messages/UpdateTransactionResponse.php
@@ -4,8 +4,23 @@ namespace PlacetoPay\MPI\Messages;
 
 class UpdateTransactionResponse
 {
-    public static function loadFromResult(array $result): self
-    {
+    protected $id;
+    protected $amount;
+    protected $payment;
 
+    public function __construct(array $data)
+    {
+        $this->id = $data['id'] ?? null;
+        $this->amount = $data['amount'] ?? null;
+        $this->payment = $data['payment'] ?? null;
+    }
+
+    public static function loadFromResult(array $result, $id = null): self
+    {
+        return new self([
+            'id' => $id,
+            'amount' => $result['amount'] ?? null,
+            'payment' => $result['payment'] ?? null,
+        ]);
     }
 }
