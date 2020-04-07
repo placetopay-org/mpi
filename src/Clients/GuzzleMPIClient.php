762ffe24775e112e46a83eff6b805d022a9cccf3
Diego Calle <dnetix@gmail.com>
2020-04-06 22:22:37 -0500

    Refactor guzzle client

--- src/Clients/GuzzleMPIClient.php
+++ src/Clients/GuzzleMPIClient.php
@@ -3,9 +3,10 @@
 namespace PlacetoPay\MPI\Clients;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\Exception\ClientException;
-use GuzzleHttp\Exception\ServerException;
+use GuzzleHttp\Exception\BadResponseException;
 use PlacetoPay\MPI\Contracts\MPIClient;
+use PlacetoPay\MPI\Contracts\MPIException;
+use PlacetoPay\MPI\Exceptions\ServiceException;
 
 class GuzzleMPIClient implements MPIClient
 {
@@ -21,29 +22,35 @@ class GuzzleMPIClient implements MPIClient
     public function execute($url, $method, $data, $headers)
     {
         $client = new Client();
-        if (in_array($method, ['POST', 'PATCH'])) {
-            $response = $client->request($method, $url, [
-                'headers' => $headers,
-                'json' => $data,
-            ]);
-        } else {
-            if ($method == 'GET') {
-                $response = $client->get($url, [
+        try {
+            if (in_array($method, ['POST', 'PATCH'])) {
+                $response = $client->request($method, $url, [
                     'headers' => $headers,
                     'json' => $data,
                 ]);
             } else {
-                if ($method == 'PUT') {
-                    $response = $client->put($url, [
+                if ($method == 'GET') {
+                    $response = $client->get($url, [
                         'headers' => $headers,
                         'json' => $data,
                     ]);
                 } else {
-                    throw new \Exception("No valid method for this request");
+                    if ($method == 'PUT') {
+                        $response = $client->put($url, [
+                            'headers' => $headers,
+                            'json' => $data,
+                        ]);
+                    } else {
+                        throw new \Exception("No valid method for this request");
+                    }
                 }
             }
+            $response = $response->getBody()->getContents();
+            return json_decode($response, true);
+        } catch (BadResponseException $e) {
+            throw new MPIException($e->getResponse()->getBody()->getContents());
+        } catch (\Exception $exception) {
+            throw new ServiceException($exception->getMessage());
         }
-        $response = $response->getBody()->getContents();
-        return json_decode($response, true);
     }
 }
