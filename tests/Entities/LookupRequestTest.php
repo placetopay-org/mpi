e16a1fa829620a66c904277312fda61210deded2
Diego Calle <dnetix@gmail.com>
2021-07-26 21:38:03 -0500

    Add test for the desired behaviour

---
+++ tests/Entities/LookupRequestTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace PlacetoPay\MPI\Tests\Entities;
+
+use PlacetoPay\MPI\Messages\LookupResponseVersionOne;
+use PlacetoPay\MPI\Requests\LookupRequestVersionOne;
+use PlacetoPay\MPI\Requests\LookupRequestVersionTwo;
+use PlacetoPay\MPI\Tests\BaseTestCase;
+
+class LookupRequestTest extends BaseTestCase
+{
+    public function testItParsesSuccessfullyARequest()
+    {
+        $data = [
+            'payer' => [
+                'name' => 'Diego',
+                'surname' => 'Calle',
+            ],
+            'card' => [
+                'number' => '4111111111111111',
+                'expirationYear' => '2021',
+                'expirationMonth' => '08',
+            ],
+            'amount' => 100,
+            'reference' => 'TEST_REFERENCE',
+            'currency' => 'USD',
+            'redirectUrl' => 'https://dnetix.co/ping',
+            'disableRedirect' => false,
+            'userAgent' => 'Mozilla Firefox all the other things here 3.583',
+        ];
+
+        $request = new LookupRequestVersionTwo($data);
+
+        $this->assertEquals([
+            'acctNumber' => '4111111111111111',
+            'cardExpiryDate' => '2108',
+            'purchaseAmount' => 100,
+            'purchaseCurrency' => 'USD',
+            'redirectURI' => 'https://dnetix.co/ping',
+            'threeDSAuthenticationInd' => '01',
+            'reference' => 'TEST_REFERENCE',
+            'cardholderName' => 'Diego',
+        ], $request->toArray());
+    }
+
+    public function testItHandlesASubscriptionRequest()
+    {
+        $data = [
+            'payer' => [
+                'name' => 'Diego',
+                'surname' => 'Calle',
+            ],
+            'card' => [
+                'number' => '4111111111111111',
+                'expirationYear' => '2021',
+                'expirationMonth' => '08',
+            ],
+            'amount' => null,
+            'reference' => 'TEST_REFERENCE',
+            'currency' => null,
+            'redirectUrl' => 'https://dnetix.co/ping',
+            'disableRedirect' => false,
+            'userAgent' => 'Mozilla Firefox all the other things here 3.583',
+        ];
+
+        $request = new LookupRequestVersionTwo($data);
+
+        $this->assertEquals([
+            'acctNumber' => '4111111111111111',
+            'cardExpiryDate' => '2108',
+            'redirectURI' => 'https://dnetix.co/ping',
+            'threeDSAuthenticationInd' => '04',
+            'reference' => 'TEST_REFERENCE',
+            'cardholderName' => 'Diego',
+            'messageCategory' => '02',
+        ], $request->toArray());
+    }
+}
