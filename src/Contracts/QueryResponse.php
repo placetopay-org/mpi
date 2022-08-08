58223940b2485786d6246773b4e34ace1e285867
Diego Calle <dnetix@gmail.com>
2022-08-08 16:39:34 -0500

    Improve toArray for queryResponse

--- src/Contracts/QueryResponse.php
+++ src/Contracts/QueryResponse.php
@@ -25,6 +25,28 @@ abstract class QueryResponse extends MPIBaseMessage
 
     abstract public function extra(): array;
 
+    abstract public function validSignature(): bool;
+
+    public function toArray(): array
+    {
+        $data = [
+            'id' => $this->id(),
+            'enrolled' => $this->enrolled(),
+            'authenticated' => $this->authenticated(),
+            'validSignature' => $this->validSignature(),
+            'eci' => $this->eci(),
+            'cavv' => $this->cavv(),
+            'xid' => $this->xid(),
+            'version' => $this->version(),
+        ];
+
+        if ($this->version() === MPI::VERSION_TWO) {
+            $data['extra'] = $this->extra();
+        }
+
+        return $data;
+    }
+
     public static function readVersion(string $version): string
     {
         if (substr($version, 0, 1) === '1') {
